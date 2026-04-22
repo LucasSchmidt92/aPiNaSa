2824f01becc1326275e335fa35058b6a1e2ee362
Lucas Schmidt <lucas_schmidt92@hotmail.com>
2026-04-22 20:01:15 -0300

    Add files via upload

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+
+// Componente simples de cabeçalho
+export default function Header() {
+  return (
+    <header style={{ textAlign: "center", marginBottom: "20px" }}>
+      <h1>🌌 Escolha uma data importante na sua vida</h1>
+      <p>Encontre ai no seu calendario</p>
+    </header>
+  );
+}
---
+++ src/components/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="loader">
+      <p>Calma ai que tu buscou uma data muito distante</p>
+    </div>
+  );
+}
